707541c1e243aafdf6e24177f8f135e2b7e4b8a4
Pavan Vishnu <meharwad@usc.edu>
2020-02-11 19:34:40 -0700

    Create insertionSort.c

---
+++ Arrays/insertionSort.c
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                         Insertion Sort
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define N (5)
+
+void insertionSort(int A[], int n)
+{
+    int i;
+    
+    for(i = 1; i < n; i++)
+    {
+        int hole = i;
+        int data = A[i];
+        int j = i-1;
+        
+        while(A[j] > data)
+        {
+            A[hole--] = A[j--];
+        }
+        
+        A[hole] = data;
+    }
+}
+
+
+int main()
+{
+    int A[N] = {5,1,2,4,3};
+    insertionSort(A, N);
+    
+    int i;
+    for(i = 0; i < N; i++)
+        printf("%d  ", A[i]);
+    printf("\n");
+    return 0;
+}
